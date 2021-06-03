1238f2feab53a8d3a5ea34380bfcf6eef6744d2e
mkeeeeey <mkeeeeey@kakao.com>
2021-06-03 13:55:04 +0900

    commTester 210603 - 2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import multiprocessing
 import sys
 from PyQt5.QtWidgets import *
 from Window.MainWindow import MainWindow
 
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     app = QApplication(sys.argv)
     view = MainWindow()
     view.show()
